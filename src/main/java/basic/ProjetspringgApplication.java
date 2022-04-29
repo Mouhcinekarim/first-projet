d4b2f0a4dd6e1e28f6d024dd4a3e19bed26eedad
unknown <mouhssinkarim34@gmail.com>
2022-04-29 20:38:12 +0100

    first commit

---
+++ src/main/java/basic/ProjetspringgApplication.java
@@ -0,0 +1,13 @@
+package basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetspringgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetspringgApplication.class, args);
+	}
+
+}
